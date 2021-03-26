25c965e3d26eeccb490397b5ae8974ccd37712b5
Gabriel Locia <locciaa1@gmail.com>
2021-03-26 11:00:20 -0600

    Agregando un poco de diseño, No es mucho pero es trabajo honesto :)

--- app.py
+++ app.py
@@ -73,36 +73,39 @@ def home():
 @app.route('/mutation', methods=['POST'])
 def mutacion():
     #Conexion a la base de datos 
-    connection  = pymysql.connect(user='locia@mutationserver',
-                       password='Gal14695238',
-                       database='mutations',
-                       host='mutationserver.mysql.database.azure.com',
-                       cursorclass=pymysql.cursors.DictCursor,
-                       ssl= {'ca':'./BaltimoreCyberTrustRoot.crt.pem'})
+    if request.method == 'POST':
+        connection  = pymysql.connect(user='locia@mutationserver',
+                        password='Gal14695238',
+                        database='mutations',
+                        host='mutationserver.mysql.database.azure.com',
+                        cursorclass=pymysql.cursors.DictCursor,
+                        ssl= {'ca':'./BaltimoreCyberTrustRoot.crt.pem'})
 
+        
+        dn = request.json['dna'] #obtiene el archivo json de la url 
+        #comprueba si el adn tiene mutaciones
+        mutations, cout_mutations, cout_no_mutations = hasMutation(dn)
+
+        cont[0] = cont[0] + cout_mutations #posicion 0 para mutaciones
+        cont[1] = cont[1] + cout_no_mutations #posicion 1 para no mutaciones
+
+        Stradn = "".join(dn)    
     
-    dn = request.json['dna'] #obtiene el archivo json de la url 
-    #comprueba si el adn tiene mutaciones
-    mutations, cout_mutations, cout_no_mutations = hasMutation(dn)
 
-    cont[0] = cont[0] + cout_mutations #posicion 0 para mutaciones
-    cont[1] = cont[1] + cout_no_mutations #posicion 1 para no mutaciones
-
-    Stradn = "".join(dn)    
-   
-
-    if mutations:
-        with connection:
-            with connection.cursor() as cursor:
-                cursor.execute('INSERT INTO adn(id,DNA,Mutation) VALUES(%s,%s,%s)',(0,Stradn,cout_mutations))
-            connection.commit()
-        return 'Se han encontrado mutaciones', 200, {'Content-Type':'text/plain'}
-    else:
-        with connection:
-            with connection.cursor() as cursor:
-                cursor.execute('INSERT INTO adn(id,DNA,Mutation) VALUES(%s,%s,%s)',(0,Stradn,cout_mutations))
-            connection.commit()
-        return 'No se encotraron mutacinones', 403, {'Content-Type':'text/plain'}
+        if mutations:
+            with connection:
+                with connection.cursor() as cursor:
+                    cursor.execute('INSERT INTO adn(id,DNA,Mutation) VALUES(%s,%s,%s)',(0,Stradn,cout_mutations))
+                connection.commit()
+            return 'Se han encontrado mutaciones', 200, {'Content-Type':'text/plain'}
+        else:
+            with connection:
+                with connection.cursor() as cursor:
+                    cursor.execute('INSERT INTO adn(id,DNA,Mutation) VALUES(%s,%s,%s)',(0,Stradn,cout_mutations))
+                connection.commit()
+            return 'No se encotraron mutacinones', 403, {'Content-Type':'text/plain'}
+    
+    return render_template('secueciaMutacion.html')
    
 
 @app.route('/status')
